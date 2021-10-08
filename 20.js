897663747c9eab2b186766508c81b01af326b5e7
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-10-08 10:52:20 -0400

    add 20.js

---
+++ 20.js
@@ -0,0 +1,34 @@
+/**
+ * Crie uma função que receba dois parametros, um array e um número
+ * apresente o array apensa se o tamanho do array seja igual ao número
+ * trate os erros com try catch
+ */
+
+function tratarErro(arr, num){
+    try {
+        if(!arr || !num) throw new ReferenceError("Necessário passar os parâmetros.")
+        if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo Object")
+        if(typeof num !== 'number') throw new TypeError("num precisa ser um número")
+        if( arr.length !== num) throw new RangeError("O tamanho do array é diferente de num")
+        return arr
+    } catch(e) {
+        if(e instanceof ReferenceError){
+            return(`
+                Este é um ReferenceError
+                ${e.message}
+            `)
+        } else if(e instanceof TypeError) {
+            return(`
+                Este é um TypeError
+                ${e.message}
+            `)
+        } else {
+            return(`
+                Este é um RangeError
+                ${e.message}
+            `)
+        }
+    }//try - catch
+}//trataErro
+
+console.log(tratarErro(5, 8))
