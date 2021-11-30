3d0fe4a29a750630c2339eacd63234d85a5b2672
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-11-30 16:49:29 -0400

    verificando se numero digitado é < que 0

--- multiplos.js
+++ multiplos.js
@@ -1,6 +1,20 @@
 const num1 = prompt("Digite o primeiro número: ")
 const num2 = prompt("Digite o segundo número: ")
-const resultado = Number(num1) / Number(num2)
+let resultado 
+
+if(num2 > num1) {
+    if( num2 < 0) {
+        resultado = Number(num1) / Number(num2)
+    } else {
+        resultado = Number(num2) / Number(num1)
+    }
+    console.log("num2 >")
+} else {
+    resultado = Number(num1) / Number(num2)
+    console.log("num2 <")
+}
+
+console.log(resultado)
 
 if(Number.isInteger(resultado)) {
     console.log("São Multiplos")
